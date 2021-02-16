20ea2662ef59be3209e0376e64cdec7e971dd449
salmansaeed <pfc.salman@gmail.com>
2021-02-16 17:38:24 -0500

    Files with Current Testing of Function Call

--- test/weekCategory.component.test.tsx
+++ test/weekCategory.component.test.tsx
@@ -1,16 +1,39 @@
-import 'react-native';
+//import 'jsdom-global/register';
+import  React from 'react'; 
+import {View, Text} from 'react-native'; 
+
 import 'jest-enzyme';
 import '@testing-library/jest-dom';
 import Enzyme from 'enzyme';
-import React from 'react';
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import CategoryAdd from './testComp';
+
+
+configure({ adapter: new Adapter() })
+
+
 
 describe('tests for weekCategory.component', ()=>{
     test('that category displays correctly', ()=>{
-
+       
+        const wrapper =  shallow(<CategoryAdd/>); 
+        expect(wrapper.debug().length).toBeGreaterThan(0);  
+       // console.log(wrapper.debug()); 
     });
     
     test('that the button calls deleteCategory from categoryService and refreshes list', () =>{
+        const mockFunction = jest.fn();
 
-    });
+        const wrapper =  shallow(<CategoryAdd/>); 
+        console.log(wrapper.containsAllMatchingElements([<CategoryAdd />])); 
+        // const instancePress = wrapper.instance;
+        //  console.log(instancePress.toString());
+        // expect(instancePress).toHaveBeenCalled();
+        //  const fnCall = jest.spyOn(wrapper.instance,'myPress'); 
+        // // expect(fnCall).toBeCalled();
+        // console.log(fnCall);
+        });
 
 });
