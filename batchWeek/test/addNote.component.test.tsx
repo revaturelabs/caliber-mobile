dcea6366346755351d33a1607fbe6215f6c30bf0
Janine Monique Geraldizo <jm.geraldizo@gmail.com>
2021-02-16 15:24:26 -0500

    finishing up test for addNote component

--- batchWeek/test/addNote.component.test.tsx
+++ batchWeek/test/addNote.component.test.tsx
@@ -4,15 +4,23 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import AddNoteComponent from '../addNote.component';
-import { Button } from 'react-native';
+import { Button, TextInput } from 'react-native';
 
 test('test adding an overall note', () => {
     const wrapper = Enzyme.mount(<AddNoteComponent />);
     const mockFn = jest.fn();
-    
+    const mockChange = jest.fn();
+
+    const input = Enzyme.shallow(
+        <TextInput onChange={mockChange} />
+    );
+    input.simulate('change', { target: { value: 'something' } });
+    expect(mockChange).toHaveBeenCalled();
+
     const tree = Enzyme.shallow(
         <Button title='button test' onPress={mockFn} />
-      );
-      tree.simulate('click');
-      expect(mockFn).toHaveBeenCalled();
+    );
+    tree.simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+
 });
