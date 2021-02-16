e688e6a2ddbbb69382095ca7125f85fd3282d8a3
KirstenWood <kirstenhope98@gmail.com>
2021-02-16 12:53:07 -0500

    set up tests of and service

---
+++ test/weekCategoryList.test.ts
@@ -0,0 +1,16 @@
+import 'react-native';
+import 'jest-enzyme';
+import '@testing-library/jest-dom';
+import Enzyme from 'enzyme';
+import React from 'react';
+
+describe('tests for weekCategoryList', ()=>{
+    test('that nothing is displayed if there are no categories for the week', ()=>{
+
+    });
+    
+    test('that categories display correctly if there are categories for the week', () =>{
+
+    });
+
+});
