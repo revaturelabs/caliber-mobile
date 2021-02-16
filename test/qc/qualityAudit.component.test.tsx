87de63455df90e63225e7c93ee8366a625dcebff
Lilith Moore <jessejm123@gmail.com>
2021-02-16 15:28:40 -0500

    Quality Audit component tests

---
+++ test/qc/qualityAudit.component.test.tsx
@@ -0,0 +1,44 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import Enzyme from 'enzyme';
+import 'react-native';
+import 'jest-enzyme';
+import '@testing-library/jest-dom';
+
+import QualityAuditComponent from '../../qc/qualityAudit.component';
+
+describe('Quality Audit Component tests', ()=> {
+    let wrapper: any;
+    beforeAll(()=> {
+        wrapper = Enzyme.mount(<QualityAuditComponent/>);
+    })
+
+    test('if the component renders batch dropdowns', ()=>{
+        const batchSelection = wrapper.findWhere((node: any) => node.prop('testID') === 'batchSelection').first();
+        expect(batchSelection).toExist();
+    });
+    
+    test('if the component renders week tabs', ()=>{
+        const weeks = wrapper.findWhere((node: any) => node.prop('testID') === 'weeks').first();
+        expect(weeks).toExist();
+    });
+
+    test('if the component renders categories', ()=>{
+        const categories = wrapper.findWhere((node: any) => node.prop('testID') === 'categories').first();
+        expect(categories).toExist();
+    });
+
+    test('if the component renders associate table', ()=>{
+        const associateTable = wrapper.findWhere((node: any) => node.prop('testID') === 'associateTable').first();
+        expect(associateTable).toExist();
+    });
+
+    test('if the component renders overall qc status and notes', ()=>{
+        const overallStatus = wrapper.findWhere((node: any) => node.prop('testID') === 'overallStatus').first();
+        expect(overallStatus).toExist();
+        const overallNotes = wrapper.findWhere((node: any) => node.prop('testID') === 'overallNotes').first();
+        expect(overallNotes).toExist();
+    });
+});
