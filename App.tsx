47e996b7e1e906764924b7632c90d7c9f37eced3
Cynth <cynthia.enciso28@gmail.com>
2021-02-18 11:15:14 -0600

    moved navigationcontainer

--- App.tsx
+++ App.tsx
@@ -4,12 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import {Router} from './router/router';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Router />
+        <NavigationContainer>
+          <Router />
+        </NavigationContainer>
       </View>
     </Provider>
   );
--- router/router.tsx
+++ router/router.tsx
@@ -1,27 +1,24 @@
-import { NavigationContainer } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import ManageCategories from '../categories/manageCategories'
+import ManageCategories from '../categories/manageCategories';
   
 export type StackParam = {
   ManageCategories: undefined;
 };
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator<StackParam>();
 
 export function Router() {
     return (
-      <NavigationContainer>
         <Stack.Navigator initialRouteName='Login'>
           <Stack.Screen name='Login' component={} />
           <Stack.Screen name='Home' component={} />
           <Stack.Screen name='Manage Batch' component={} />
           <Stack.Screen name='Assess Batch' component={} />
-          <Stack.Screen name='Manage Categories' component={ManageCategories} />
+          <Stack.Screen name='ManageCategories' component={ManageCategories} />
           <Stack.Screen name='Quality Audit' component={} />
           <Stack.Screen name='Reports' component={} />
         </Stack.Navigator>
-      </NavigationContainer>
     );
   }
 
