40562860a0d1c2119f895bcef3ba9dd0e7affc28
Cynth <cynthia.enciso28@gmail.com>
2021-02-18 11:00:19 -0600

    created router component

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import {Router} from './router/router';
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <Router />
       </View>
     </Provider>
   );
