97d7b67cd45d6d2a40b01d81c3613aacfadd9dc8
Cynth <cynthia.enciso28@gmail.com>
2021-02-18 12:42:26 -0600

    removed style property

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import {Router} from './router/router';
+import Router from './router/router';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
@@ -22,7 +21,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
