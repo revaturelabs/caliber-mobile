981ca543db17f6e3274e761af8e7ea5ca0787dca
Tngtater9 <jtater1@gmail.com>
2021-02-18 14:44:59 -0500

    removed duplicated imporst

--- App.tsx
+++ App.tsx
@@ -2,27 +2,25 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
 import store from './store/store';
 
-import store from './store/store';
 import RouterComponent from './router/router.component';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-        <NavigationContainer>
-            <RouterComponent/>
-        </NavigationContainer>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<NavigationContainer>
+				<RouterComponent />
+			</NavigationContainer>
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
