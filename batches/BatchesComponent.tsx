7ad9739d58bd8bd812e2565162cd70debfb3906a
Robert Thompson <farellth@gmail.com>
2021-02-19 21:16:56 -0500

    updated batches component for new store/reducer

--- batches/BatchesComponent.tsx
+++ batches/BatchesComponent.tsx
@@ -5,18 +5,22 @@ import { FlatList } from 'react-native-gesture-handler';
 import { Picker } from '@react-native-picker/picker';
 import { useNavigation } from '@react-navigation/native';
 
-import { BatchState } from '../store/store';
+import { RootState } from '../store/store';
 import { getBatches } from '../store/actions';
+import batchReducer from '../store/batchReducer';
 import batchService from './BatchService';
 import Batch from './Batch';
 
 export default function BatchesComponent() {
     const navigation = useNavigation();
     const dispatch = useDispatch();
-    const batches = useSelector((state: BatchState) => state.batches);
-    const [filter, setFilter] = useState('');
+    const batches = useSelector((state: RootState) => state.batchReducer.batches);
+    const [yearFilter, setYearFilter] = useState('');
+    const [batchFilter, setBatchFilter] = useState('');
     const [list, setList] = useState<Batch[]>([]);
     const keyExtractor = (item: object, index: number) => { return index.toString(); }
+
+    const currentYear: number = new Date().getFullYear();
 
     const trainerEmail = 'mock1027.employee74df14df-5842-4811-a57c-be9836537a40@mock.com';
 
@@ -33,8 +37,17 @@ export default function BatchesComponent() {
         )
     }
 
+    function handleYearFilter(newBatchId: any) {
+        setYearFilter(newBatchId);
+        if(newBatchId){
+            setList(batches.filter(batch=>batch.batchId == newBatchId));
+        } else {
+            setList(batches);
+        }
+    }
+
     function handleFilter(newBatchId: any) {
-        setFilter(newBatchId);
+        setBatchFilter(newBatchId);
         if(newBatchId){
             setList(batches.filter(batch=>batch.batchId == newBatchId));
         } else {
@@ -48,28 +61,33 @@ export default function BatchesComponent() {
         });
     }, []);
 
-    console.log(batches);
-
     return (
         <View>
             {(() => {
-                if (batches) {
+                if (batches[0]) {
                     return (
                         <View>
                             <Picker
-                                selectedValue={filter}
+                                selectedValue={yearFilter}
+                                onValueChange={handleYearFilter}>
+                                <Picker.Item
+                                    label='Year'
+                                    value=''/>
+                            </Picker>
+                            <Picker
+                                selectedValue={batchFilter}
                                 onValueChange={handleFilter}>
                                 <Picker.Item
                                     label='None'
                                     value=''/>
-                                {batches.length > 0 && batches.map((batch) => {
+                                {batches.map((batch) => {
                                     return <Picker.Item
                                         key={batch.batchId}
                                         label={batch.name}
                                         value={batch.batchId}/>
                                 })}
                             </Picker>
-                            <FlatList data={filter == '' ? null : list} renderItem={batchPreview} keyExtractor={keyExtractor}/>
+                            <FlatList data={batchFilter == '' ? null : list} renderItem={batchPreview} keyExtractor={keyExtractor}/>
                         </View>
                     )
                 } else {
--- router/router.component.tsx
+++ router/router.component.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text } from 'react-native-elements';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StackHeaderOptions } from '@react-navigation/stack/lib/typescript/src/types';
+
+import HomeComponent from '../HomeComponent';
 import BatchesComponent from '../batches/BatchesComponent';
 
 export type StackParams = {
@@ -18,6 +20,11 @@ const Stack = createStackNavigator();
 export default function RouterComponent(props: any) {
     return (
         <Stack.Navigator initialRouteName='Batches'>
+            <Stack.Screen
+                name='Home'
+                component={HomeComponent}
+                options={headerOptions}
+            />
             <Stack.Screen
                 name='Batches'
                 component={BatchesComponent}
