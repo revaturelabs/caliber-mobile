ae6d921c2ea58a616b3895bcd10f9288025ee4d8
Robert Thompson <farellth@gmail.com>
2021-02-22 15:34:20 -0500

    changed picker to filter by year and quarter for a given trainer

--- batches/BatchesComponent.tsx
+++ batches/BatchesComponent.tsx
@@ -7,7 +7,6 @@ import { useNavigation } from '@react-navigation/native';
 
 import { RootState } from '../store/store';
 import { getBatches } from '../store/actions';
-import batchReducer from '../store/batchReducer';
 import batchService from './BatchService';
 import Batch from './Batch';
 
@@ -16,13 +15,15 @@ export default function BatchesComponent() {
     const dispatch = useDispatch();
     const batches = useSelector((state: RootState) => state.batchReducer.batches);
     const [yearFilter, setYearFilter] = useState('');
+    const [quarterFilter, setQuarterFilter] = useState('');
     const [batchFilter, setBatchFilter] = useState('');
     const [list, setList] = useState<Batch[]>([]);
+    const [chosenBatch, setChosenBatch] = useState<Batch[]>([]);
     const keyExtractor = (item: object, index: number) => { return index.toString(); }
 
-    const currentYear: number = new Date().getFullYear();
+    const validYears: any = [2020, 2021];
 
-    const trainerEmail = 'mock1027.employee74df14df-5842-4811-a57c-be9836537a40@mock.com';
+    const trainer = {email: 'mock1005.employee7c90a542-e70e-4db5-be8b-629e62f851c5@mock.com', firstName: 'Mock 1005', lastName: 'Employee 1005'};
 
     const batchPreview = (params: any) => {
         return (
@@ -37,29 +38,69 @@ export default function BatchesComponent() {
         )
     }
 
-    function handleYearFilter(newBatchId: any) {
-        setYearFilter(newBatchId);
-        if(newBatchId){
-            setList(batches.filter(batch=>batch.batchId == newBatchId));
-        } else {
-            setList(batches);
-        }
-    }
-
-    function handleFilter(newBatchId: any) {
-        setBatchFilter(newBatchId);
-        if(newBatchId){
-            setList(batches.filter(batch=>batch.batchId == newBatchId));
-        } else {
-            setList(batches);
-        }
-    }
-
+    // Updates the state with the all of the chosen trainer's batches
     useEffect(() => {
-        batchService.getBatchesByTrainerEmail(trainerEmail).then((batchesResp) => {
+        batchService.getBatchesByTrainerEmail(trainer.email).then((batchesResp) => {
             dispatch(getBatches(batchesResp));
         });
     }, []);
+
+    // Sets the year and resets the other dropdowns
+    function handleYearFilter(year: any) {
+        setYearFilter(year);
+        setQuarterFilter('');
+        setBatchFilter('');
+    }
+
+    // Sets the quarter and resets the batch dropdown
+    function handleQuarterFilter(quarter: any) {
+        setQuarterFilter(quarter);
+        setBatchFilter('');
+        if (quarter && yearFilter) {
+            setList(batches.filter( batch => {
+                if (batch.startDate.indexOf(String(yearFilter)) >= 0) {
+                    return batch;
+                }
+            }).filter( batch => {
+                if (checkQuarter(batch.startDate) == quarter) {
+                    return batch;
+                }
+            }));
+        }
+    }
+
+    // Selects a specified batch to display
+    function handleFilter(newBatchId: any) {
+        setBatchFilter(newBatchId);
+        if(newBatchId){
+            setChosenBatch(batches.filter(batch=>batch.batchId == newBatchId));
+        } else {
+            setChosenBatch(batches);
+        }
+    }
+
+    // Checks a provided date to see which quarter it's in and returns a string representing the quarter
+    function checkQuarter(date: string) {
+        const month: number = Number(date.slice(5, 7));
+        switch (month) {
+            case 1:
+            case 2:
+            case 3:
+                return 'Q1';
+            case 4:
+            case 5:
+            case 6:
+                return 'Q2';
+            case 7:
+            case 8:
+            case 9:
+                return 'Q3';
+            case 10:
+            case 11:
+            case 12:
+                return 'Q4';
+        }
+    }
 
     return (
         <View>
@@ -73,21 +114,74 @@ export default function BatchesComponent() {
                                 <Picker.Item
                                     label='Year'
                                     value=''/>
-                            </Picker>
-                            <Picker
-                                selectedValue={batchFilter}
-                                onValueChange={handleFilter}>
-                                <Picker.Item
-                                    label='None'
-                                    value=''/>
-                                {batches.map((batch) => {
+                                {validYears.map((year: any) => {
                                     return <Picker.Item
-                                        key={batch.batchId}
-                                        label={batch.name}
-                                        value={batch.batchId}/>
+                                        key={year}
+                                        label={year}
+                                        value={year}/>
                                 })}
                             </Picker>
-                            <FlatList data={batchFilter == '' ? null : list} renderItem={batchPreview} keyExtractor={keyExtractor}/>
+                            {(() => {
+                                if (yearFilter) {
+                                    return (
+                                        <View>                                            
+                                            <Picker
+                                            selectedValue={quarterFilter}
+                                            onValueChange={handleQuarterFilter}>
+                                                <Picker.Item
+                                                    label='Quarter'
+                                                    value=''/>
+                                                <Picker.Item
+                                                    label='Q1'
+                                                    value='Q1'/>
+                                                <Picker.Item
+                                                    label='Q2'
+                                                    value='Q2'/>
+                                                <Picker.Item
+                                                    label='Q3'
+                                                    value='Q3'/>
+                                                <Picker.Item
+                                                    label='Q4'
+                                                    value='Q4'/>
+                                            </Picker>
+                                            {(() => {
+                                                if (quarterFilter) {
+                                                    return (
+                                                        <Picker
+                                                            selectedValue={batchFilter}
+                                                            onValueChange={handleFilter}>
+                                                            {(() => {
+                                                                if (list[0]) {
+                                                                    return (
+                                                                        <Picker.Item
+                                                                            label='Choose Batch'
+                                                                            value=''/>
+                                                                    )
+                                                                } else {
+                                                                    return (
+                                                                        <Picker.Item
+                                                                            label='No Batches Found'
+                                                                            value=''/>
+                                                                    )
+                                                                }
+                                                            })()}
+                                                            {list.map((batch) => {
+                                                                return (
+                                                                    <Picker.Item
+                                                                        key={batch.batchId}
+                                                                        label={trainer.firstName + ' ' + trainer.lastName + ' - ' + batch.skill + ' - ' + batch.startDate}
+                                                                        value={batch.batchId}/>
+                                                                )
+                                                            })}
+                                                        </Picker>
+                                                    )
+                                                }
+                                            })()}
+                                        </View>
+                                    )
+                                }
+                            })()}
+                            <FlatList data={batchFilter == '' ? null : chosenBatch} renderItem={batchPreview} keyExtractor={keyExtractor}/>
                         </View>
                     )
                 } else {
