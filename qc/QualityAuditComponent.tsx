625c1a8c5b90a0aea1c2cecb3ed0f5f07260399d
Cassandra Terry <cassandra34terry@gmail.com>
2021-02-24 13:21:27 -0600

    add week

--- qc/QualityAuditComponent.tsx
+++ qc/QualityAuditComponent.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import AddWeek from '../batchWeek/AddWeek/addWeek.component';
 import WeekSelectionComponent from '../batchWeek/WeekSelectionComponent';
 
 /**
  * The parent component for the QC Quality Audit Page; will conatin the selection of specific
  * batches and weeks, the table of associates with status and notes, and overall week status and
- * notes. 
+ * notes.
  */
 export default function QualityAuditComponent() {
     return (
         <>
             <WeekSelectionComponent/>
+            <AddWeek />
         </>
     );
 }
