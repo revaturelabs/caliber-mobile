732d5336a29b54f911f54857f1edab07f9919cc9
relytnroh <relytnroh@gmail.com>
2021-03-02 14:19:19 -0800

    reportsComponentUpdate

--- batchPage/BatchPageService.ts
+++ batchPage/BatchPageService.ts
@@ -1,18 +1,20 @@
-import axios from "axios";
-
-
+import axios from 'axios';
 
 class BatchPageService {
+  private URI: string;
+  constructor() {
+    // URL of the API server that stores our Lambdas.
+    this.URI = 'https://d3e1hb8u20.execute-api.us-east-1.amazonaws.com/default';
+  }
 
-    private URI: string;
-    constructor() {
-        // URL of the express server
-        this.URI = 'https://d3e1hb8u20.execute-api.us-east-1.amazonaws.com/default'
-    }
-
-    async getAssociates(batchID:string, token:string):Promise<[]> {
-        return axios.get(this.URI+"qc/batch/"+batchID+"/associates",{ headers: {'Authorization': `Bearer ${token}`}}).then(result => result.data).catch((err) => {console.error(err)});
-    }
+  async getAssociates(batchID: string, token: string): Promise<[]> {
+    return axios
+      .get(`${this.URI}/qc/batches/${batchID}`, {headers: {'Authorization': `Bearer ${token}`}})
+      .then((result) => result.data)
+      .catch((err) => {
+        console.error(err);
+      });
+  }
 }
 
-export default new BatchPageService()
+export default new BatchPageService();
