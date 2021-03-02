04157fce76d6fffc31b6fb287e528cc4255aea22
kathrync23 <kathrynclark16@augustana.edu>
2021-03-02 15:03:56 -0600

    fixing Home navigation error

--- router/MainStackNavigator.component.tsx
+++ router/MainStackNavigator.component.tsx
@@ -8,32 +8,32 @@ import LogoutComponent from '../user/Logout';
 import { useDispatch, useSelector } from 'react-redux';
 import CategoryService from '../categoriesFeature/CategoryService';
 import {
-    GetActive,
-    GetStale,
+  GetActive,
+  GetStale,
 } from '../store/categoriesFeature/CategoryActions';
 import { ReducerState } from '../store/store';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Image } from 'react-native-elements';
 import LoginComponent from '../user/Login';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 enableScreens();
 
 const Stack = createStackNavigator();
 
 export interface MenuProp {
-    navigation: any;
+  navigation: any;
 }
 /**
  * Shows only the Revature logo for the header
  */
 export const loginHeaderOptions = {
-    headerTitle: () => (
-        <Image
-            style={{ width: 165, height: 50, margin: 30 }}
-            source={require('./rev-logo.png')}
-        />
-    ),
+  headerTitle: () => (
+    <Image
+      style={{ width: 165, height: 50, margin: 30 }}
+      source={require('./rev-logo.png')}
+    />
+  ),
 };
 
 /**
@@ -42,23 +42,22 @@ export const loginHeaderOptions = {
  * @param navigation - navigation prop to open the drawer
  */
 export function generalHeaderOptions(navigation: any) {
-    return {
-        headerTitle: () => (
-            <Image
-                style={{ width: 165, height: 50, margin: 30 }}
-                source={require('./rev-logo.png')}
-            />
-        ),
-        headerLeft: () => (
-            <Icon.Button
-                name='ios-menu'
-                size={25}
-                color='#72A4C2'
-                backgroundColor='#fff'
-                onPress={() => navigation.openDrawer()}
-            ></Icon.Button>
-        ),
-    };
+  return {
+    headerTitle: () => (
+      <Image
+        style={{ width: 165, height: 50, margin: 30 }}
+        source={require('./rev-logo.png')}
+      />
+    ),
+    headerLeft: () => (
+      <Icon.Button
+        name='ios-menu'
+        size={25}
+        color='#72A4C2'
+        backgroundColor='#fff'
+        onPress={() => navigation.openDrawer()}></Icon.Button>
+    ),
+  };
 }
 
 /**
@@ -69,33 +68,33 @@ export function generalHeaderOptions(navigation: any) {
  * Forgot Password - allows the user to reset their password.
  */
 const LoginStackNavigator = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name='Login'
-                component={LoginComponent}
-                options={loginHeaderOptions}
-            />
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Login'
+        component={LoginComponent}
+        options={loginHeaderOptions}
+      />
 
-            <Stack.Screen
-                name="'ForgotPassword'"
-                component={ForgotPassword}
-                options={loginHeaderOptions}
-            />
-        </Stack.Navigator>
-    );
+      <Stack.Screen
+        name='ForgotPassword'
+        component={ForgotPassword}
+        options={loginHeaderOptions}
+      />
+    </Stack.Navigator>
+  );
 };
 
 const HomeStack = ({ navigation }: MenuProp) => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name='Home'
-                component={Home}
-                options={generalHeaderOptions(navigation)}
-            />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Home'
+        component={Home}
+        options={generalHeaderOptions(navigation)}
+      />
+    </Stack.Navigator>
+  );
 };
 
 /**
@@ -103,15 +102,15 @@ const HomeStack = ({ navigation }: MenuProp) => {
  * @param navigation - navigation prop to open the drawer
  */
 const ReportStack = ({ navigation }: MenuProp) => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name='Reports'
-                component={UnderDevelopmentComponent}
-                options={generalHeaderOptions(navigation)}
-            />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Reports'
+        component={UnderDevelopmentComponent}
+        options={generalHeaderOptions(navigation)}
+      />
+    </Stack.Navigator>
+  );
 };
 
 /**
@@ -121,34 +120,34 @@ const ReportStack = ({ navigation }: MenuProp) => {
  * @param navigation - navigation prop to open the drawer
  */
 const ManagementStack = ({ navigation }: MenuProp) => {
-    const dispatch = useDispatch();
-    // authorizer state
-    const currentUser = useSelector(
-        (state: ReducerState) => state.userReducer.user
-    );
-    const token = currentUser.token;
-    const [rend, setRend] = useState(false);
-    // get manage category table data
-    useEffect(() => {
-        async function getCategoryFunc() {
-            const active = await CategoryService.getCategories(token, true);
-            const stale = await CategoryService.getCategories(token, false);
-            dispatch(GetActive(active));
-            dispatch(GetStale(stale));
-        }
-        setRend(true);
-        getCategoryFunc();
-    }, []);
+  const dispatch = useDispatch();
+  // authorizer state
+  const currentUser = useSelector(
+    (state: ReducerState) => state.userReducer.user
+  );
+  const token = currentUser.token;
+  const [rend, setRend] = useState(false);
+  // get manage category table data
+  useEffect(() => {
+    async function getCategoryFunc() {
+      const active = await CategoryService.getCategories(token, true);
+      const stale = await CategoryService.getCategories(token, false);
+      dispatch(GetActive(active));
+      dispatch(GetStale(stale));
+    }
+    setRend(true);
+    getCategoryFunc();
+  }, []);
 
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name='Management'
-                component={ManageCategories}
-                options={generalHeaderOptions(navigation)}
-            />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Management'
+        component={ManageCategories}
+        options={generalHeaderOptions(navigation)}
+      />
+    </Stack.Navigator>
+  );
 };
 
 /**
@@ -157,21 +156,21 @@ const ManagementStack = ({ navigation }: MenuProp) => {
  * @param navigation - navigation prop to open the drawer
  */
 const LogoutStack = ({ navigation }: MenuProp) => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name='Logout'
-                component={LogoutComponent}
-                options={generalHeaderOptions(navigation)}
-            />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Logout'
+        component={LogoutComponent}
+        options={generalHeaderOptions(navigation)}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export {
-    LoginStackNavigator,
-    HomeStack,
-    ReportStack,
-    ManagementStack,
-    LogoutStack,
+  LoginStackNavigator,
+  HomeStack,
+  ReportStack,
+  ManagementStack,
+  LogoutStack,
 };
